dbe66582781190378c7897cc992829a3c9f86ec9
hieuhp01 <46868429+hieuhp01@users.noreply.github.com>
2023-06-04 10:50:21 +0200

    small change

--- src/Components/Role_Selector.js
+++ src/Components/Role_Selector.js
@@ -1,18 +1,18 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import { v1 } from 'uuid'
-export const Role_Select = ({membership,actions}) =>
-{
+import { Hourglass } from 'react-bootstrap-icons';
+export const Role_Select = ({ membership, actions }) => {
     console.log(membership)
-    const current_role=membership?.user.roles?.filter((item) => item.group.id===membership.group.id && item.valid===true)
+    const current_role = membership?.user.roles?.filter((item) => item.group.id === membership.group.id && item.valid === true)
     console.log(current_role)
-    const roles=useSelector(state =>state.roles)
-    const role_list=Object.values(roles)
-    const onRoleChange=({roletype_new,membership}) =>
-    {
+    const roles = useSelector(state => state.roles)
+    const role_list = Object.values(roles)
+    const onRoleChange = ({ roletype_new, membership }) => {
         console.log(membership)
-        const new_role=
+        const new_role =
         {
             id: v1(),
             roletypeID: roletype_new.id,
@@ -35,36 +35,34 @@ export const Role_Select = ({membership,actions}) =>
         actions.onMemberUpdate(new_role_store)
 
     }
-    const Make_New_Role = () =>
-    {
+    const Make_New_Role = () => {
 
-        const [input_role,set_input_role]=useState("")
-        const onRoleMake = () =>
-        {
+        const [input_role, set_input_role] = useState("")
+        const onRoleMake = () => {
             console.log("button called")
-            const payload={
-                id:v1(),
-                name:input_role,
-                nameEn:input_role
+            const payload = {
+                id: v1(),
+                name: input_role,
+                nameEn: input_role
             }
             actions.roletype_insert(payload)
-            onRoleChange({roletype_new:payload,membership})
+            onRoleChange({ roletype_new: payload, membership })
         }
         const handleInputChange = (event) => {
             set_input_role(event.target.value);
         };
         return (
             <>
-             <input className="form-control" placeholder="new role" value={input_role} onChange={handleInputChange}/>
-             <button onClick={onRoleMake}><Hourglass></Hourglass></button>         
+                <input className="form-control" placeholder="new role" value={input_role} onChange={handleInputChange} />
+                <button onClick={onRoleMake}><Hourglass></Hourglass></button>
             </>
         )
     }
     return (
-    <DropdownButton id="dropdown-basic-button" title={current_role[current_role.length-1]?.roletype.nameEn}>
-      {role_list?.map((roletype_new)=>(<Dropdown.Item onClick={event => onRoleChange({roletype_new,membership})} >{roletype_new.nameEn}</Dropdown.Item>)     )}
-      <Make_New_Role />
-    </DropdownButton>
+        <DropdownButton id="dropdown-basic-button" title={current_role[current_role.length - 1]?.roletype.nameEn}>
+            {role_list?.map((roletype_new) => (<Dropdown.Item onClick={event => onRoleChange({ roletype_new, membership })} >{roletype_new.nameEn}</Dropdown.Item>))}
+            <Make_New_Role />
+        </DropdownButton>
     )
 }
 
